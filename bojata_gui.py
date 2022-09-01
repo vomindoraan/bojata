f45cbafe4ba7b24eae9ebca99d94fd39f02bb5fb
Konstantin Đorđević <vomindoraan@gmail.com>
2022-09-01 15:19:55 +0200

    Update GUI constants

--- bojata_gui.py
+++ bojata_gui.py
@@ -16,20 +16,28 @@ FONT_NAME = 'TkDefaultFont'
 DRAWER_COUNT = 10
 DEFAULT_LOCATION = "Kreativni distrikt, Novi Sad"
 
+PRINT_FONT_NAME = '/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf'
+PRINT_FONT = ImageFont.truetype(PRINT_FONT_NAME, 24)
+PRINT_FONT_LARGE = ImageFont.truetype(PRINT_FONT_NAME, 96)
+
 
 class BojataRoot(tk.Tk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.title('bojata')
-        self.geometry('{}x{}'.format(self.winfo_screenwidth(),
-                                     self.winfo_screenheight()))
+        self.geometry(f'{self.winfo_screenwidth()}x{self.winfo_screenheight()}')
         self.attributes('-fullscreen', True)
         self.protocol('WM_DELETE_WINDOW', exit)
         self.update()  # Update actual width and height
+
         self.pad = self.winfo_width() // 100
+        self.halfpad = (0, self.pad)
 
         tk.font.nametofont(FONT_NAME).configure(size=18)
+        self.font = (FONT_NAME, 18)
+        self.font_medium = (FONT_NAME, 24)
+        self.font_large = (FONT_NAME, 36)
 
         container = tk.Frame(self)
         container.pack(fill=tk.BOTH, expand=True)
@@ -68,17 +76,16 @@ class HomeFrame(BojataFrame):
         self.color_frame = tk.Frame(self)
         self.color_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True,
                               padx=self.root.pad, pady=self.root.pad)
-        font = (FONT_NAME, 24)
 
-        tk.Button(self, text="OČITAJ\nBOJU", font=font,
+        tk.Button(self, text="OČITAJ\nBOJU", font=self.root.font_medium,
                   padx=self.root.pad*4, pady=self.root.pad*2,
                   command=partial(root.show_frame, 'ScanFrame'))\
-            .pack(side=tk.TOP, expand=True)
+            .pack(side=tk.TOP, expand=True, padx=self.root.halfpad)
 
-        tk.Button(self, text="BAZA\nBOJA", font=font,
+        tk.Button(self, text="BAZA\nBOJA", font=self.root.font_medium,
                   padx=self.root.pad*4, pady=self.root.pad*2,
                   command=partial(root.show_frame, 'TableFrame'))\
-            .pack(side=tk.TOP, expand=True)
+            .pack(side=tk.TOP, expand=True, padx=self.root.halfpad)
 
 
 class ScanFrame(BojataFrame):
@@ -109,31 +116,31 @@ class ScanFrame(BojataFrame):
 
         f = 'hex'
         self.iv[f] = tk.StringVar(self)
-        self.il[f] = tk.Label(frame1, textvariable=self.iv[f], font=(FONT_NAME, 36))
+        self.il[f] = tk.Label(frame1, textvariable=self.iv[f], font=self.root.font_large)
         self.il[f].pack(side=tk.BOTTOM)
 
         # Right half
         frame2 = tk.Frame(self)
         frame2.grid(row=0, column=1, sticky='nsew',
-                    padx=self.root.pad, pady=self.root.pad)
+                    padx=self.root.halfpad, pady=self.root.pad)
         frame2.columnconfigure(0, weight=1)
         frame2.columnconfigure(1, weight=1)
-        pady = (0, self.root.pad)
-        font = (FONT_NAME, 18)
 
         f = 'author'
         self.il[f] = tk.Label(frame2, text="IME AUTORA ⁽*⁾")
         self.il[f].grid(row=0, column=0, columnspan=2, sticky='nw')
         self.iv[f] = tk.StringVar(self)
-        self.ie[f] = tk.Entry(frame2, textvariable=self.iv[f], font=font)
-        self.ie[f].grid(row=1, column=0, columnspan=2, sticky='we', pady=pady)
+        self.ie[f] = tk.Entry(frame2, textvariable=self.iv[f], font=self.root.font)
+        self.ie[f].grid(row=1, column=0, columnspan=2, sticky='we',
+                        pady=self.root.halfpad)
 
         f = 'name'
         self.il[f] = tk.Label(frame2, text="NAZIV BOJE")
         self.il[f].grid(row=2, column=0, columnspan=2, sticky='nw')
         self.iv[f] = tk.StringVar(self)
-        self.ie[f] = tk.Entry(frame2, textvariable=self.iv[f], font=font)
-        self.ie[f].grid(row=3, column=0, columnspan=2, sticky='we', pady=pady)
+        self.ie[f] = tk.Entry(frame2, textvariable=self.iv[f], font=self.root.font)
+        self.ie[f].grid(row=3, column=0, columnspan=2, sticky='we',
+                        pady=self.root.halfpad)
 
         f = 'category'
         self.il[f] = tk.Label(frame2, text="KATEGORIJA BOJE")
@@ -141,7 +148,8 @@ class ScanFrame(BojataFrame):
         self.iv[f] = tk.StringVar(self)
         categories = [c.value for c in bojata_db.ColorCategory]
         self.ie[f] = tk.OptionMenu(frame2, self.iv[f], "", *categories)
-        self.ie[f].grid(row=5, column=0, columnspan=2, sticky='we', pady=pady)
+        self.ie[f].grid(row=5, column=0, columnspan=2, sticky='we',
+                        pady=self.root.halfpad)
 
         f = 'drawer'
         self.il[f] = tk.Label(frame2, text="BROJ KASETE")
@@ -149,28 +157,33 @@ class ScanFrame(BojataFrame):
         self.iv[f] = tk.StringVar(self)
         drawers = range(1, DRAWER_COUNT+1)
         self.ie[f] = tk.OptionMenu(frame2, self.iv[f], "", *drawers)
-        self.ie[f].grid(row=7, column=0, columnspan=2, sticky='we', pady=pady)
+        self.ie[f].grid(row=7, column=0, columnspan=2, sticky='we',
+                        pady=self.root.halfpad)
 
         f = 'comment'
         self.il[f] = tk.Label(frame2, text="KOMENTAR")
         self.il[f].grid(row=8, column=0, columnspan=2, sticky='nw')
         self.iv[f] = tk.StringVar(self)
-        self.ie[f] = tk.Entry(frame2, textvariable=self.iv[f], font=font)
-        self.ie[f].grid(row=9, column=0, columnspan=2, sticky='we', pady=pady)
+        self.ie[f] = tk.Entry(frame2, textvariable=self.iv[f], font=self.root.font)
+        self.ie[f].grid(row=9, column=0, columnspan=2, sticky='we',
+                        pady=self.root.halfpad)
 
         f = 'location'
         self.il[f] = tk.Label(frame2, text="LOKACIJA")
         self.il[f].grid(row=10, column=0, columnspan=2, sticky='nw')
         self.iv[f] = tk.StringVar(self, DEFAULT_LOCATION)
-        self.ie[f] = tk.Entry(frame2, textvariable=self.iv[f], font=font)
-        self.ie[f].grid(row=11, column=0, columnspan=2, sticky='we', pady=pady)
+        self.ie[f] = tk.Entry(frame2, textvariable=self.iv[f], font=self.root.font)
+        self.ie[f].grid(row=11, column=0, columnspan=2, sticky='we',
+                        pady=self.root.halfpad)
 
         f = 'datetime'
         self.iv[f] = tk.StringVar(self, datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 
-        tk.Button(frame2, text="✔", fg='green', font=font, command=self.submit)\
+        tk.Button(frame2, text="✔", fg='green', font=self.root.font_medium,
+                  command=self.submit)\
             .grid(row=12, column=0, sticky='we', ipady=self.root.pad)
-        tk.Button(frame2, text="❌", fg='red', font=font, command=self.cancel)\
+        tk.Button(frame2, text="❌", fg='red', font=self.root.font_medium,
+                  command=self.cancel)\
             .grid(row=12, column=1, sticky='we', ipady=self.root.pad)
 
     def submit(self):
@@ -203,9 +216,6 @@ class ScanFrame(BojataFrame):
         if answer:
             bojata.start_printing(self.scanned_color, self.generate_image())
 
-    LARGE_FONT = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf', 96)
-    SMALL_FONT = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf', 24)
-
     def generate_image(self):
         with Image.open('template_rev0.6.png') as img:
             img.resize((874, 1240))  # A5 @ 150 PPI
@@ -213,35 +223,35 @@ class ScanFrame(BojataFrame):
 
             bojata.draw_swatch(draw, self.scanned_color, x=80, y=56, w=256, h=168)
             text = textwrap.fill(self.scanned_color, width=50, max_lines=1)
-            draw.text((432, 96), text, font=self.LARGE_FONT, fill=self.scanned_color)
+            draw.text((432, 96), text, font=PRINT_FONT_LARGE, fill=self.scanned_color)
 
             value = self.iv['author'].get()
             text = textwrap.fill(value, width=50, max_lines=1)
-            draw.text((96, 308), text, font=self.SMALL_FONT, fill='black')
+            draw.text((96, 308), text, font=PRINT_FONT, fill='black')
 
             value = self.iv['name'].get()
             text = textwrap.fill(value, width=50, max_lines=1)
-            draw.text((96, 436), text, font=self.SMALL_FONT, fill='black')
+            draw.text((96, 436), text, font=PRINT_FONT, fill='black')
 
             value = self.iv['category'].get()
             text = textwrap.fill(value, width=50, max_lines=1)
-            draw.text((96, 566), text, font=self.SMALL_FONT, fill='black')
+            draw.text((96, 566), text, font=PRINT_FONT, fill='black')
 
             value = self.iv['drawer'].get()
             text = textwrap.fill(value, width=50, max_lines=1)
-            draw.text((96, 692), text, font=self.SMALL_FONT, fill='black')
+            draw.text((96, 692), text, font=PRINT_FONT, fill='black')
 
             value = self.iv['comment'].get()
             text = textwrap.fill(value, width=50, max_lines=5)
-            draw.text((96, 820), text, font=self.SMALL_FONT, fill='black')
+            draw.text((96, 820), text, font=PRINT_FONT, fill='black')
 
             value = self.iv['location'].get()
             text = textwrap.fill(value, width=24, max_lines=3)
-            draw.text((96, 1076), text, font=self.SMALL_FONT, fill='black')
+            draw.text((96, 1076), text, font=PRINT_FONT, fill='black')
 
             value = self.iv['datetime'].get()
             text = textwrap.fill(value, width=24, max_lines=3)
-            draw.text((472, 1076), text, font=self.SMALL_FONT, fill='black')
+            draw.text((472, 1076), text, font=PRINT_FONT, fill='black')
 
             return img
 
@@ -249,8 +259,16 @@ class ScanFrame(BojataFrame):
 class TableFrame(BojataFrame):
     def __init__(self, parent, root):
         super().__init__(parent, root)
-        self.table = Table(self)
+        frame = tk.Frame(self)
+        frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True,
+                   padx=self.root.pad, pady=self.root.pad)
+        self.table = Table(frame)
         self.table.show()
+
+        tk.Button(self, text="NAZAD", font=self.root.font_medium,
+                  padx=self.root.pad*2, pady=self.root.pad,
+                  command=partial(root.show_frame, 'HomeFrame'))\
+            .pack(side=tk.TOP, pady=self.root.halfpad)
 
     def on_show_frame(self, event):
         data = bojata_db.Color.read_data()
