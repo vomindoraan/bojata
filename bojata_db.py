088350276aed2396a8d9dc540a06cabbade14761
Konstantin Đorđević <vomindoraan@gmail.com>
2026-05-02 05:33:24 +0200

    Add Base, LabelsMeta model classes in bojata_db.py
    Update to modern SQLAlchemy ORM conventions

--- bojata_db.py
+++ bojata_db.py
@@ -1,56 +1,26 @@
 import enum
 
 import pandas as pd
-from sqlalchemy import Column, DateTime, Enum, Integer, String, create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import Session
+from sqlalchemy import Enum, Integer, String, create_engine
+from sqlalchemy.orm import DeclarativeBase, Session, mapped_column
 
 
-Base = declarative_base()
+DB_URL = 'sqlite:///data/bojata.db'
+
 engine = None
 
 
-class ColorCategory(enum.Enum):
-    YELLOW = "Žuta"
-    ORANGE = "Narandžasta"
-    RED = "Crvena"
-    PINK = "Roze"
-    LIGHT_GREEN = "Svetlozelena"
-    DARK_GREEN = "Tamnozelena"
-    LIGHT_BLUE = "Svetloplava"
-    DARK_BLUE = "Tamnoplava"
-    BROWN = "Braon"
-    BLACK = "Crna"
-    OTHER = "Ostalo"
+class Base(DeclarativeBase):
+    pass
 
 
-class Color(Base):
-    __tablename__ = 'color'
+class LabelsMeta(type(Base)):
+    def __init__(cls, name, bases, attrs):
+        super().__init__(name, bases, attrs)
+        cls.__labels__ = {c.name: c.doc for c in cls.__table__.columns}
 
-    id = Column(Integer, primary_key=True)
-    author = Column(String(50), nullable=False)
-    hex = Column(String(7), nullable=False)
-    name = Column(String(50))
-    category = Column(Enum(ColorCategory))
-    object = Column(String(50))
-    comment = Column(String(250))
-    location = Column(String(72))
-    datetime = Column(String(20), nullable=False)
-
-    COLUMN_LABELS = {
-        'author':   "Ime autora",
-        'hex':      "Kôd boje",
-        'name':     "Naziv boje",
-        'category': "Kategorija boje",
-        'object':   "Skenirani predmet / Broj kasete",
-        'comment':  "Komentar",
-        'location': "Mesto",
-        'datetime': "Datum i vreme",
-    }
-
-    @classmethod
     def label_of(cls, column, annotated=True):
-        label = COLUMN_LABELS[column]
+        label = cls.__labels__[column]
         if annotated:
             label = label.upper()
             if not cls.__table__.columns[column].nullable:
@@ -58,22 +28,52 @@ class Color(Base):
         return label
 
 
+class ColorCategory(enum.Enum):
+    YELLOW      = "Žuta"
+    ORANGE      = "Narandžasta"
+    RED         = "Crvena"
+    PINK        = "Roze"
+    LIGHT_GREEN = "Svetlozelena"
+    DARK_GREEN  = "Tamnozelena"
+    LIGHT_BLUE  = "Svetloplava"
+    DARK_BLUE   = "Tamnoplava"
+    BROWN       = "Braon"
+    BLACK       = "Crna"
+    OTHER       = "Ostalo"
+
+
+class Color(Base, metaclass=LabelsMeta):
+    __tablename__ = 'color'
+
+    id       = mapped_column(Integer,     primary_key=True, doc="ID")
+    author   = mapped_column(String(50),  nullable=False,   doc="Ime autora")
+    hex      = mapped_column(String(7),   nullable=False,   doc="Kôd boje")
+    name     = mapped_column(String(50),                    doc="Naziv boje")
+    category = mapped_column(Enum(ColorCategory),           doc="Kategorija boje")
+    object   = mapped_column(String(50),                    doc="Skenirani predmet / Broj kasete")
+    comment  = mapped_column(String(250),                   doc="Komentar")
+    location = mapped_column(String(72),                    doc="Mesto")
+    datetime = mapped_column(String(20),  nullable=False,   doc="Datum i vreme")
+
     @classmethod
-    def read_data(cls, column_mapping=cls.COLUMN_LABELS):
+    def read_data(cls, column_mapping=None):
+        if column_mapping is None:
+            column_mapping = cls.__labels__
         columns = column_mapping.keys()
-        df = pd.read_sql_table(cls.__tablename__, engine,
-                               columns=columns, parse_dates=['datetime'])
+        df = pd.read_sql_table(cls.__tablename__, engine, columns=columns, parse_dates=['datetime'])
         df.rename(columns=column_mapping, inplace=True)
         return df
 
     @classmethod
-    def empty_data(cls, column_mapping=cls.COLUMN_LABELS):
+    def empty_data(cls, column_mapping=None):
+        if column_mapping is None:
+            column_mapping = cls.__labels__
         return pd.DataFrame(columns=column_mapping.values())
 
 
 def init():
     global engine
-    engine = create_engine('sqlite:///data/bojata.db', echo=True)
+    engine = create_engine(DB_URL, echo=True)
     Base.metadata.create_all(engine)
 
 
