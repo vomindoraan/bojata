0e467a7980ddfd04e3b3ce63a3e856e2a64d9671
Konstantin Đorđević <vomindoraan@gmail.com>
2026-05-10 03:46:59 +0200

    Unconditionally import bojata_lcd

--- bojata_gui.py
+++ bojata_gui.py
@@ -13,8 +13,7 @@ from PIL import Image, ImageDraw, ImageFont
 
 import bojata
 import bojata_db as db
-if bojata.LCD_ENABLED:
-    import bojata_lcd as lcd
+import bojata_lcd as lcd
 
 
 UI_FONT_NAME = 'TkDefaultFont'
